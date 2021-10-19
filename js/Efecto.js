3b40cecb54c7e1cef70cfda51d40425508cfc5b8
Ander_AAM <ander_2030@hotmail.com>
2021-10-19 01:38:46 -0500

    style dark o light , depurado

--- js/Efecto.js
+++ js/Efecto.js
@@ -1,40 +1,37 @@
-
-
 $(document).ready(function () {
-    var clic = 0;
+                    $('.button_detalle_mas').click(function () {
+                        $('.ver_mas').css({
+                            "height": "170px"
+                        });
+                        $('.card').css({
+                            "height": "100%"
+                        });
+                        $('.button_detalle_mas').hide();
+                        /*
+                        $('.button_detalle_mas').css({
+                            "display": "hide"
+                        });*/
+                        $('.button_detalle_menos').css({
+                            "display": "block"
+                        });
+                    })
 
-    $('.button_detalle_mas').click(function(){
-        $('.ver_mas').css({
-            "height": "170px"
-        });
-        $('.Marco_Secundario').css({
-            "height": "1000px"
-        });
-        $('.button_detalle_mas').css({
-            "display": "none"
-        });
-        $('.button_detalle_menos').css({
-            "display": "block"
-        });
-    })
-
-    $('.button_detalle_menos').click(function(){
-        $('.ver_mas').css({
-            "height": "90px"
-        });
-        $('.Marco_Secundario').css({
-            "height": "900px"
-        });
-        $('.button_detalle_mas').css({
-            "display": "block"
-        });
-        $('.button_detalle_menos').css({
-            "display": "none"
-        });
-    })
-
-})
-
-
+                    $('.button_detalle_menos').click(function () {
+                        $('.ver_mas').css({
+                            "height": "70px",
+                        });
+                        $('.card').css({
+                            "height": "100%"
+                        });
+                        $('.button_detalle_mas').show();
+                        /*
+                        $('.button_detalle_mas').css({
+                            "display": "block"
+                        });*/
+                        $('.button_detalle_menos').css({
+                            "display": "none"
+                        });
+                    })
 
 
+                })
