5bd411658b2490acf79b03dddd7f060e431f6c7c
Ander_AAM <ander_2030@hotmail.com>
2021-10-20 15:02:31 -0500

    cambios para el responsive

--- js/Efecto.js
+++ js/Efecto.js
@@ -1,37 +1,13 @@
 $(document).ready(function () {
-                    $('.button_detalle_mas').click(function () {
-                        $('.ver_mas').css({
-                            "height": "170px"
-                        });
-                        $('.card').css({
-                            "height": "100%"
-                        });
-                        $('.button_detalle_mas').hide();
-                        /*
-                        $('.button_detalle_mas').css({
-                            "display": "hide"
-                        });*/
-                        $('.button_detalle_menos').css({
-                            "display": "block"
-                        });
-                    })
+    
+    $('.button_detalle_mas').click(function () {
+        $('.Extendido').css({"display":"inline"})
+        $('.button_detalle_mas').css({"display":"none"})
+    });
 
-                    $('.button_detalle_menos').click(function () {
-                        $('.ver_mas').css({
-                            "height": "70px",
-                        });
-                        $('.card').css({
-                            "height": "100%"
-                        });
-                        $('.button_detalle_mas').show();
-                        /*
-                        $('.button_detalle_mas').css({
-                            "display": "block"
-                        });*/
-                        $('.button_detalle_menos').css({
-                            "display": "none"
-                        });
-                    })
+    $('.button_detalle_menos').click(function () {
+        $('.Extendido').css({"display":"none"})
+        $('.button_detalle_mas').css({"display":"inline"})
+    });
 
-
-                })
+})
